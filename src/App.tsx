925694f36874b692758daca8604c55a83e381512
Arman <arm.dev.292@gmail.com>
2023-03-22 17:03:09 +0400

    added Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import "./App.css"
+import BoardComponent from "./components/BoardComponent";
 const App = () => {
   return (
-    <div>
-
+    <div className='app'>
+      <BoardComponent />
     </div>
   );
 };
